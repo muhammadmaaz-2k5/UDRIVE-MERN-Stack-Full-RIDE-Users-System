862061a5a7a17020b7e8f3711173bc99014f190d
Muhammad Maaz <22pwbcs0933@uetpeshawar.edu.pk>
2026-08-04 03:11:32 +0500

    fixed backend connection with mongodb

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
 
+// Override DNS servers to fix querySrv ECONNREFUSED on some networks
+dns.setServers(['1.1.1.1', '8.8.8.8']);
 
 function connectToDb() {
-    mongoose.connect(process.env.DB_CONNECT
+    mongoose.connect(process.env.MONGO_URI || process.env.DB_CONNECT
     ).then(() => {
         console.log('Connected to DB');
     }).catch(err => console.log(err));
